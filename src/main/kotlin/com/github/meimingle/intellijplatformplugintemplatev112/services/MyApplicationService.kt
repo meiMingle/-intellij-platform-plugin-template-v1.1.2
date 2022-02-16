1ffc22c62cdbfb2fe8e937a53664cc47148741db
GitHub Action <action@github.com>
2022-02-16 01:00:37 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/meimingle/intellijplatformplugintemplatev112/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.meimingle.intellijplatformplugintemplatev112.services
+
+import com.github.meimingle.intellijplatformplugintemplatev112.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
